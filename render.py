cd4dcf3c899c8a5b4df95f5d1d9aba18e26007ca
Rockwell <rockwellgulassa@gmail.com>
2026-01-27 18:05:36 +0100

    feat: add minimal visualization function for displaying a grid state

--- render.py
+++ render.py
@@ -1,1 +1,39 @@
 # Code for rendering the simulation in the terminal
+import numpy as np
+from typing import Dict, Any
+
+
+def display_grid_state(
+    grid: np.array, 
+    symbols: Dict[Any, str] = {1: 'X', 0: '_', 'sep': '|'}
+) -> None:
+    """
+    Takes in a 2D numpy array and prints a text visual of the array.
+
+    Parameters:
+    ----------
+    grid : np.array
+        2D numpy array containing the grid state to be visualized
+    symbols : dict
+        Dictionary with the keys 0, 1, and 'sep' used to determine the characters used for the visual
+
+    Returns:
+    ----------
+    None
+    """
+
+    # --- Printing grid "roof" ---
+    # Compute line length to know how many characters to have in the first row
+    line_length: int = len(grid[0]) * (len(symbols[0]) + len(symbols['sep'])) + len(symbols['sep'])
+    # Concatenate underscores into "roof" of grid
+    print(' _'*int(line_length/2))
+
+    # --- Displaying Matrix ---
+    # Loop through rows in the grid
+    for row in grid:
+        # Convert numbers into symbols from the symbols dict for displaying
+        line: list[str] = [symbols[cell] for cell in row]
+        # Concatenate cell symbols with separator
+        line: str = symbols['sep'] + symbols['sep'].join(line) + symbols['sep']
+        # Print row
+        print(line)
