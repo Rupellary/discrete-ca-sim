11db442e616fefe0f658718788c60f5941b5b62b
Rockwell <rockwellgulassa@gmail.com>
2026-02-02 15:23:17 +0100

    feat: add skeleton of typer CLI

--- main.py
+++ main.py
@@ -2,18 +2,50 @@
 import numpy as np
 from render import display_grid_state
 from sim import CellularAutomata
+import typer
 
-# Hypothetical grid state to be used for various test
-test_state: np.array = np.array([
-    [0, 0, 0, 0, 0, 1],
-    [1, 1, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 1, 0, 0],
-    [1, 1, 0, 0, 0, 0]
-])
-# Testing display function
-display_grid_state(test_state)
-ca = CellularAutomata(grid_state=test_state)
-ca.step()
-display_grid_state(ca.grid_state)
+# --------------------
+# --- TESTING AREA ---
+# --------------------
+
+# # Hypothetical grid state to be used for various test
+# test_state: np.array = np.array([
+#     [0, 0, 0, 0, 0, 1],
+#     [1, 1, 0, 0, 1, 0],
+#     [0, 0, 0, 0, 0, 0],
+#     [0, 0, 0, 1, 0, 0],
+#     [0, 0, 0, 1, 0, 0],
+#     [1, 1, 0, 0, 0, 0]
+# ])
+# # Testing display function
+# display_grid_state(test_state)
+# ca = CellularAutomata(grid_state=test_state)
+# ca.step()
+# display_grid_state(ca.grid_state)
+
+
+# -----------
+# --- CLI ---
+# -----------
+
+# [to team] working off of Typer Docs: https://typer.tiangolo.com/tutorial/arguments/optional/#an-alternative-cli-argument-declaration
+
+app = typer.Typer()
+
+@app.command()
+def main(
+    rule_string: str = typer.Option(
+        help="""
+            Update rule specified as sets of neighbor counts that result in surival (alive->alive) and those that result in birth (dead->alive)
+            Written as S_B_. 
+            Example: S23B3 
+            The above is the rule for Conway's Game of Life. It means that living cells with 2 or 3 living neighbors stay alive and dead cells with 3 neighbors become alive.
+        """)
+):
+    """
+    Example Command: $ python main.py --rule-string S23B3
+    """
+    print(rule_string)
+
+if __name__ == "__main__":
+    app()
