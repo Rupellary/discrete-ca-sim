f4c5926560ab01f5f48c38504f13765467b8d114
Rockwell <rockwellgulassa@gmail.com>
2026-02-02 13:58:29 +0100

    feat: add skeleton of CA class. Note: changed from thresholds for transition specification to sets of neighbor counts to allow for more flexibility.

--- sim.py
+++ sim.py
@@ -1,1 +1,31 @@
 # Code for running the simulation
+import numpy as np
+
+class CellularAutomata:
+    """
+    Cellular automata, including grid as an attribute and update rule as a method.
+    Updates states based on the count of living neighbors.
+
+    Attributes
+    ----------
+    grid_state : np.ndarray
+        Current state of grid of cells, alive cells store 1s, dead cells store 0s.
+    surive_set : set
+        Set of neighbor counts that result in living cells remaining alive.
+    birth_set : set
+        Set of neighbor counts that result in dead cells transitioning to alive.    
+    """
+
+    def __init__(
+        self,
+        grid_state: np.ndarray,
+        survive_set: set,
+        birth_set: set,
+    ):
+    
+        self.grid_state = grid_state
+        self.survive_set = survive_set
+        self.birth_set = birth_set
+
+    def update_rule(self):
+        pass
